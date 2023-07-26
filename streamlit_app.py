2aab19ed23aeda63321cdfdeeadd88e7475c88cc
Paul Oyelabi <oyelabipaul@gmail.com>
2023-07-26 13:50:52 +0100

    removed camera input implementation

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,16 +25,16 @@ st.sidebar.write("## Upload or take picture from your camera :gear:")
 if "uploaded_img_changed" not in st.session_state:
     st.session_state.uploaded_img_changed = 0
 
-if "camera_img_changed" not in st.session_state:
-    st.session_state.camera_img_changed = 0
+# if "camera_img_changed" not in st.session_state:
+#     st.session_state.camera_img_changed = 0
 
 def uploaded_on_change_callback():
-    st.session_state.uploaded_img_changed = 1
-    st.session_state.camera_img_changed = 0
+    st.session_state.uploaded_img_changed ^= 1
+    # st.session_state.camera_img_changed = 0
 
-def camera_on_change_callback():
-    st.session_state.uploaded_img_changed = 0
-    st.session_state.camera_img_changed = 1
+# def camera_on_change_callback():
+#     st.session_state.uploaded_img_changed = 0
+#     st.session_state.camera_img_changed = 1
 
 def convert_image(img):
     buffer = io.BytesIO()
@@ -44,13 +44,13 @@ def convert_image(img):
 
     return byte_img
 
-def camera_img_callback(img_input): 
-    # img_input = st.session_state.camera_img
-    img = Image.open(img_input) 
-    img_col.write("Camera Input Image :camera:")
-    img_col.image(img)
-    encoded_img = base64.b64encode(img_input.getvalue()).decode()
-    classify_image(encoded_img)
+# def camera_img_callback(img_input): 
+#     # img_input = st.session_state.camera_img
+#     img = Image.open(img_input) 
+#     img_col.write("Camera Input Image :camera:")
+#     img_col.image(img)
+#     encoded_img = base64.b64encode(img_input.getvalue()).decode()
+#     classify_image(encoded_img)
 
 def file_upload_callback(img_input): 
     # img_input = st.session_state.uploaded_img
@@ -86,15 +86,15 @@ uploaded_img = st.sidebar.file_uploader(
                 help="Ensure that the uploaded image is focused on the area of the affected skin.",
                 on_change=uploaded_on_change_callback
             )
-camera_img = st.sidebar.camera_input(
-                label="Take picture of your skin",
-                help="Ensure the center of the image is on the area of the affected skin.",
-                on_change=camera_on_change_callback
-            )
+# camera_img = st.sidebar.camera_input(
+#                 label="Take picture of your skin",
+#                 help="Ensure the center of the image is on the area of the affected skin.",
+#                 on_change=camera_on_change_callback
+#             )
 
-# if uploaded_img:
-#     file_upload_callback(uploaded_img)
-if st.session_state.uploaded_img_changed and uploaded_img:
+if uploaded_img:
     file_upload_callback(uploaded_img)
-elif st.session_state.camera_img_changed and camera_img:
-    camera_img_callback(camera_img)
+# if st.session_state.uploaded_img_changed and uploaded_img:
+#     file_upload_callback(uploaded_img)
+# elif st.session_state.camera_img_changed and camera_img:
+#     camera_img_callback(camera_img)
